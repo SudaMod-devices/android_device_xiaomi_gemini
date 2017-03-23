34466d4670a01b10444f74423c103096b0e3a196
ghbhaha <ghbhaha@gmail.com>
2017-03-23 16:29:13 +0800

    Merge branch 'cm-14.1' into sm-3.0
    Change-Id: I93b70370d04a436c896aeb38c8dd7db342cd79f6

--- device.mk
+++ device.mk
@@ -58,3 +58,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+
+WITH_GELLO_SOURCE := true
